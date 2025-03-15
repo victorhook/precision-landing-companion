2a4c1a577d456ee0e5b961a84a6ab2fa5e386f9e
Victor <victorkrook96@gmail.com>
2025-03-15 10:38:38 +0100

    Added debug logging to UDP server on init

--- src/transport_udp.cpp
+++ src/transport_udp.cpp
@@ -1,6 +1,6 @@
 #include "transport_udp.h"
 #include <string.h>
-
+#include "log.h"
 
 TransportUDP::TransportUDP(const int port)
 : port(port), initialized(false)
@@ -12,6 +12,7 @@ void TransportUDP::init(const char* remoteIP)
 {
     strncpy(this->remoteIP, remoteIP, 16);
     this->remoteIP[16] = '\0';
+    debug("UDP server starting at %s:%d", this->remoteIP, port);
     doInit();
     initialized = true;
 }
